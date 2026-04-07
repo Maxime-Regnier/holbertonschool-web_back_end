a1d61cf92b3a64984862a7d0162e3c9976ac9f8e
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-07 16:17:26 +0200

    Add docstrings to floor function module

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+"""Module containing a typed floor function."""
 
 import math
 
 
 def floor(n: float) -> int:
+    """Return the floor of a float as an integer."""
     return math.floor(n)
