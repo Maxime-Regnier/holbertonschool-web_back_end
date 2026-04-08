8cf6a0570caa8dbb2786f97ee668894765761e9b
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-08 12:09:37 +0200

    Fix formatting in make_multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float], float]:    
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by multiplier"""
     def multiply(n: float) -> float:
         return n * multiplier
