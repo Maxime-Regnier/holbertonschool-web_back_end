dbc4f43c583be2ea08611d89cf3dac33bb31f447
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-08 10:26:49 +0200

    Fix pycodestyle formatting in sum_list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,6 @@
 from typing import List
 
 
-def sum_list(input_list: List[float]) -> float:    
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats"""
     return sum(input_list)
