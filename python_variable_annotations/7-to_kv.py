05aca3b49855392c33654654f3a12c4252971034
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-08 11:47:55 +0200

    Fix formatting in task 7 to_kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:    
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple with string k and the square of v as float"""
     return (k, v ** 2)
