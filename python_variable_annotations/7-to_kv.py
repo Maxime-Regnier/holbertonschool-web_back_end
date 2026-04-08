73a81e19ea96a7f0022909548961be0384c2cf7e
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-08 11:45:34 +0200

    Add docstrings to to_kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Module that contains the to_kv function"""
+
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:    
+    """Returns a tuple with string k and the square of v as float"""
     return (k, v ** 2)
