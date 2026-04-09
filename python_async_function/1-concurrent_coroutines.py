0e88901ff63d55c0ef8c400e6d390458f629a3f0
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-09 10:52:59 +0200

    feat(async): commit pending 1- exercise files

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+wait_random = __import__ ('0-basic_async_syntax').wait_random
 async def wait_n (n: int, max_delay: int) -> List[float]:
     tasks = [wait_random(max_dalay) for _ in range(n)]
     for completed_task in asyncio.as_completed(tasks):
