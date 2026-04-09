a2c9e9c8729ca2e4754afa6ce7a8812ae3a30c57
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-09 11:06:06 +0200

    fix(async-function): correct wait_n implementation in exercise 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import List
+<<<<<<< HEAD
 wait_random = __import__ ('0-basic_async_syntax').wait_random
 async def wait_n (n: int, max_delay: int) -> List[float]:
+=======
+wait_random = __import__('0-basic_async_syntax').wait_random
+async def wait_n(n: int, max_delay: int) -> List[float]:
+>>>>>>> a41202b (fix(async-function): correct wait_n implementation in exercise 1)
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
     for completed_task in asyncio.as_completed(tasks):
