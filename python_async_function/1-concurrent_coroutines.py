1d2abda1421556d60ec7596620f6585df7023c6f
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-09 10:58:44 +0200

    feat(async): add pending async exercises files

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,9 @@ import asyncio
 from typing import List
 wait_random = __import__ ('0-basic_async_syntax').wait_random
 async def wait_n (n: int, max_delay: int) -> List[float]:
-    tasks = [wait_random(max_dalay) for _ in range(n)]
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
     for completed_task in asyncio.as_completed(tasks):
         delay = await completed_task
-        delays = []
-        for completed_task in asyncio.as_completed(tasks):
-            delay = await completed_task
-            delays.append(delay)
-            return delays
+        delays.append(delay)
+    return delays
