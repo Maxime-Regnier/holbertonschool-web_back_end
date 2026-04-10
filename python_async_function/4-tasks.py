303b27bf3e2e9fdfc19689027e44d494dec98649
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-10 16:48:43 +0200

    Fix pycodestyle error (E302)

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
+
+
 async def task_wait_n(n: int, max_delay: int):
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
