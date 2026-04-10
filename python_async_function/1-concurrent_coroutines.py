45b1e19f7c15d9e10840b519dca9aead1eba64e5
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-10 12:09:23 +0200

    Revert "fix(async-function): correct wait_n implementation in exercise 1"

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,17 @@
 
 import asyncio
 from typing import List
+<<<<<<< HEAD
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times and return delays in ascending order."""
 
+=======
+wait_random = __import__ ('0-basic_async_syntax').wait_random
+async def wait_n (n: int, max_delay: int) -> List[float]:
+>>>>>>> parent of a2c9e9c (fix(async-function): correct wait_n implementation in exercise 1)
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
     for completed_task in asyncio.as_completed(tasks):
