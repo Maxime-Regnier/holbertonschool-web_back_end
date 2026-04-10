35370230e0591b4b57db00c1cc2c37ef68221742
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-10 17:38:57 +0200

    Simplify async_generator to match Holberton requirements (remove type hints) and Fix pycodestyle: add missing newline at end of file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,9 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
