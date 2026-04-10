704bd14e8e97d2c5edc3a211c426a48dc1c046f3
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-10 16:24:26 +0200

    "docs: Add docstrings and fix pycodestyle errors in measure_time"

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,6 +2,8 @@
 import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
 def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
