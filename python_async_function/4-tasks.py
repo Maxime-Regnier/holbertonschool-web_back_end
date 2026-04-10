20b8dbdfdcc7b9a03e93d061ff338f2c3c65a40e
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-10 16:45:17 +0200

    Implement task_wait_n using task_wait_random and asyncio.as_completed

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+async def task_wait_n(n: int, max_delay: int):
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        delays.append(result)
