a833161d216ae6d6b68f4093da18dd3a7d39a971
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-21 15:06:40 +0200

    style: fix PEP8 formatting issues

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -2,14 +2,20 @@
 import csv
 import math
 from typing import List, Dict, Any, Tuple
+
+
 def index_range(page: int, page_size: int) -> tuple:
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
+
+
 class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
         self.__dataset = None
+
     def dataset(self) -> List[List]:
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -17,6 +23,7 @@ class Server:
                 dataset = [row for row in reader]
                 self.__dataset = dataset[1:]
         return self.__dataset
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
@@ -25,6 +32,7 @@ class Server:
         if start >= len(data):
             return []
         return data[start:end]
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         data = self.get_page(page, page_size)
         total_items = len(self.dataset())
