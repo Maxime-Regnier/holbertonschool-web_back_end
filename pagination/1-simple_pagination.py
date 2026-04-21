5a175f15178140d9050d3ca039704efc7ec70793
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-21 10:47:01 +0200

    fix(pagination): apply latest updates to simple pagination implementation

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -14,14 +14,14 @@ class Server:
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-            dataset = [row for row in reader]
+                dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-            return self.__dataset
+        return self.__dataset
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-                assert isinstance(page, int) and page > 0
-                assert isinstance(page_size, int) and page_size > 0
-                start, end = index_range(page, page_size)
-                data = self.dataset()
-                if start >= len(data):
-                    return []
-                return data[start:end] 
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start >= len(data):
+            return []
+        return data[start:end] 
