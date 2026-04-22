636a88b6f6c9bf9677f6c34d73c47e351293e3d2
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-22 11:46:14 +0200

    docs(pagination): add docstrings to deletion-resilient pagination server

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -7,14 +7,18 @@ import math
 from typing import List, Dict
 
 
-class Server:
+class Server:    
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
         self.__indexed_dataset = None
 
-    def dataset(self) -> List[List]:
+    def dataset(self) -> List[List]:        
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -22,7 +26,9 @@ class Server:
             self.__dataset = dataset[1:]
         return self.__dataset
 
-    def indexed_dataset(self) -> Dict[int, List]:
+    def indexed_dataset(self) -> Dict[int, List]:        
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -32,6 +38,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Get hypermedia pagination index
+        """
         assert index >= 0 and index < len(self.indexed_dataset())
         dataset = self.indexed_dataset()
         data = []
