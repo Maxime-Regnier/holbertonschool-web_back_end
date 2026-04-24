357993441ee6c841297ab582cfa433beb116d13d
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-24 14:56:06 +0200

    docs(nosql): document task 9 insert helper and add related pycache

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""Module to insert a new document in a MongoDB collection"""
+
+
 def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+
+    Args:
+        mongo_collection: pymongo collection object
+        **kwargs: keyword arguments representing the document fields
+
+    Returns:
+        The _id of the newly inserted document
+    """
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
