cbdf9479c954333e479e30fa85df7dae0193c214
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-24 15:11:01 +0200

    docs(nosql): add module and function documentation for update_topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
+"""Module to update topics of a school document in MongoDB"""
+
+
 def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+
+    Args:
+        mongo_collection: pymongo collection object
+        name: school name to update (string)
+        topics: list of topics approached in the school (list of strings)
+
+    Returns:
+        None
+    """
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
