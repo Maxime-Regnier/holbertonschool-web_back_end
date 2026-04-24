208c70175bb5006276483fe165c455c5ac158378
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-24 15:51:39 +0200

    docs(nosql): add module and function documentation for schools_by_topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+"""Module to find schools by topic in MongoDB"""
+
+
 def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic
+
+    Args:
+        mongo_collection: pymongo collection object
+        topic: topic to search for (string)
+
+    Returns:
+        List of schools that have the specified topic
+    """
     return list(mongo_collection.find({"topics": topic}))
