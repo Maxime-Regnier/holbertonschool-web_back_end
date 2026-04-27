e95c8d87f2edd414d6824b738684a740541f7cd3
Maxime-Regnier <11493@holbertonstudents.com>
2026-04-27 12:04:55 +0200

    Fix block scoped variable shadowing

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,7 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
-    // block scoped
+    const task = true;
+    const task2 = false;
   }
   return [task, task2];
 }
